1cfdd0833ccd0d33794412619306bdf38ef174b7
Silverlan <silverlann@gmail.com>
2020-09-14 09:54:33 +0200

    Added pixel offset parameters for writing image data

--- include/image/prosper_image.hpp
+++ include/image/prosper_image.hpp
@@ -51,7 +51,7 @@ namespace prosper
 		virtual DeviceSize GetAlignment() const=0;
 		virtual const void *GetInternalHandle() const=0;
 
-		virtual bool WriteImageData(uint32_t w,uint32_t h,uint32_t layerIndex,uint32_t mipLevel,uint64_t size,const uint8_t *data)=0;
+		virtual bool WriteImageData(uint32_t x,uint32_t y,uint32_t w,uint32_t h,uint32_t layerIndex,uint32_t mipLevel,uint64_t size,const uint8_t *data)=0;
 		virtual bool Map(DeviceSize offset,DeviceSize size,void **outPtr=nullptr)=0;
 		virtual bool Unmap()=0;
 		std::shared_ptr<IImage> Copy(prosper::ICommandBuffer &cmd,const util::ImageCreateInfo &copyCreateInfo);
